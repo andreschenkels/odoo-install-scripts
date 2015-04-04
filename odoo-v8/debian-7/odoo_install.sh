2342d0cb1535b61dbbfea0a3f3e0f22c6c8a0793
Aman Gautam <aman@codemymobile.com>
2015-04-04 10:36:34 +0530

    ADDED --depth 1 to avoid downloading any history.

--- odoo-v8/debian-7/odoo_install.sh
+++ odoo-v8/debian-7/odoo_install.sh
@@ -72,7 +72,7 @@ chown $OE_USER:$OE_USER /var/log/$OE_USER
 # Install ODOO
 #--------------------------------------------------
 echo -e "\n==== Installing ODOO Server ===="
-git clone --branch $OE_VERSION https://www.github.com/odoo/odoo $OE_HOME_EXT/
+git clone --depth 1 --branch $OE_VERSION https://www.github.com/odoo/odoo $OE_HOME_EXT/
 
 echo -e "\n---- Create custom module directory ----"
 su $OE_USER -c "mkdir $OE_HOME/custom"
