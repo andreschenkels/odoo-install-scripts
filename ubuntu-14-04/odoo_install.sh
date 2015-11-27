c66360a9a8ac7d11638a022c7b72b7ded7983a7a
André Schenkels <a.schenkels@ictstudio.eu>
2015-11-27 17:18:02 +0100

    Locales

--- ubuntu-14-04/odoo_install.sh
+++ ubuntu-14-04/odoo_install.sh
@@ -36,9 +36,9 @@ echo -e "\n---- Update Server ----"
 sudo apt-get update
 sudo apt-get upgrade -y
 sudo apt-get install -y locales
-sudo export LANGUAGE=en_US.UTF-8
-sudo export LANG=en_US.UTF-8
-sudo export LC_ALL=en_US.UTF-8
+export LANGUAGE=en_US.UTF-8
+export LANG=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
 sudo locale-gen en_US.UTF-8
 sudo dpkg-reconfigure locales
 
