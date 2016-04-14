4cdcf025fbb4337f6c72e0b667f9ad36ff2c989f
Martin Brehmer <git@mbrehmer.de>
2016-04-14 11:48:51 +0200

    [ADD] Document head for the script file

---
+++ ubuntu-14-04/odoo_install_nightly.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#################################################################################
+# Script for Installation: ODOO nightly builds on Ubuntu 14.04 LST
+# Author: (c) Martin Brehmer 2016
+#--------------------------------------------------------------------------------
+#
+# This script will install ODOO from the package sources for the nightly builds
+# on a clean Ubuntu 14.04 Server
+#
+#--------------------------------------------------------------------------------
+#
+# USAGE: (sudo) odoo_install_nightly.sh
+#
+# EXAMPLE sudo ./odoo_install_nightly.sh
+#
+#################################################################################
