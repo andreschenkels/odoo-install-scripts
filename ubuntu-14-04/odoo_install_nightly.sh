43c8674e04c646831607424a8f8ddd107693956c
Martin Brehmer <git@mbrehmer.de>
2016-05-20 15:14:43 +0200

    [FIX] Download the right version of wkhtmltox

--- ubuntu-14-04/odoo_install_nightly.sh
+++ ubuntu-14-04/odoo_install_nightly.sh
@@ -50,21 +50,43 @@ apt-get update && apt-get dist-upgrade -y
 echo -e "\n---- Install PostgreSQL ----"
 apt-get install postgresql -y
 
-echo -e "\n---- Install and link wkhtml as needed for odoo 8.0 ----"
-if [[ $OS_MACHINE_TYPE == "x86_64" ]]; then
-	wget -c http://download.gna.org/wkhtmltopdf/0.12/0.12.2.1/wkhtmltox-0.12.2.1_linux-trusty-amd64.deb && \
-	dpkg -i wkhtmltox-0.12.2.1_linux-trusty-amd64.deb
-	if [[ $? -ne 0 ]];then
-		apt-get install -f -y && \
+echo -e "\n---- Install and link wkhtml as needed for odoo ----"
+if [[ $OE_VERSION == "9.0" ]];then
+	if [[ $OS_MACHINE_TYPE == "x86_64" ]]; then
+		wget -c http://download.gna.org/wkhtmltopdf/0.12/0.12.2.1/wkhtmltox-0.12.2.1_linux-trusty-amd64.deb && \
 		dpkg -i wkhtmltox-0.12.2.1_linux-trusty-amd64.deb
+		if [[ $? -ne 0 ]];then
+			apt-get install -f -y && \
+			dpkg -i wkhtmltox-0.12.2.1_linux-trusty-amd64.deb
+		fi
+	else
+		wget -c http://download.gna.org/wkhtmltopdf/0.12/0.12.2.1/wkhtmltox-0.12.2.1_linux-trusty-i386.deb && \
+		dpkg -i wkhtmltox-0.12.2.1_linux-trusty-i386.deb
+		if [[ $? -ne 0 ]];then
+			apt-get install -f -y && \
+			dpkg -i wkhtmltox-0.12.2.1_linux-trusty-i386.deb
+		fi
+	fi
+elif [[ $EO_VERSION == "8.0" ]]
+   if [[ $OS_MACHINE_TYPE == "x86_64" ]]; then
+		wget -c http://download.gna.org/wkhtmltopdf/0.12/0.12.1/wkhtmltox-0.12.1_linux-trusty-amd64.deb && \
+		dpkg -i wkhtmltox-0.12.1_linux-trusty-amd64.deb
+		if [[ $? -ne 0 ]];then
+			apt-get install -f -y && \
+			dpkg -i wkhtmltox-0.12.1_linux-trusty-amd64.deb
+		fi
+	else
+		wget -c http://download.gna.org/wkhtmltopdf/0.12/0.12.1/wkhtmltox-0.12.1_linux-trusty-i386.deb && \
+		dpkg -i wkhtmltox-0.12.1_linux-trusty-i386.deb
+		if [[ $? -ne 0 ]];then
+			apt-get install -f -y && \
+			dpkg -i wkhtmltox-0.12.1_linux-trusty-i386.deb
+		fi
 	fi
 else
-	wget -c http://download.gna.org/wkhtmltopdf/0.12/0.12.2.1/wkhtmltox-0.12.2.1_linux-trusty-i386.deb && \
-	dpkg -i wkhtmltox-0.12.2.1_linux-trusty-i386.deb
-	if [[ $? -ne 0 ]];then
-		apt-get install -f -y && \
-		dpkg -i wkhtmltox-0.12.2.1_linux-trusty-i386.deb
-	fi
+	echo -e "\n Unsopported version number for odoo" >&2
+   echo "USAGE: $0 [8.0|9.0]"
+	exit 1
 fi
 if [[ $? -eq 0 ]]; then
 	ln -s /usr/local/bin/wkhtmltopdf /usr/bin/wkhtmltopdf
